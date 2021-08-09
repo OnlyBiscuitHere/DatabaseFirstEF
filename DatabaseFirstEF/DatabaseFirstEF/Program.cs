3f77f02e2623cd57f9ebb2766d8ff0da73b4413e
Benjamin <h8bmf@hotmail.co.uk>
2021-08-09 15:43:49 +0100

    v1

---
+++ DatabaseFirstEF/DatabaseFirstEF/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DatabaseFirstEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DatabaseFirstEF/DatabaseFirstEF/SpartansTable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace DatabaseFirstEF
+{
+    public partial class SpartansTable
+    {
+        public int SpartanId { get; set; }
+        public string Title { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string University { get; set; }
+        public string Degree { get; set; }
+        public string Course { get; set; }
+        public string Grade { get; set; }
+        public int? Mark { get; set; }
+    }
+}
